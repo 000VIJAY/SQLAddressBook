be315270b379adf2e95e617bcec4c8c1866f8363
000VIJAY <vijay893689@gmail.com>
2022-07-22 15:58:58 +0530

    [Vijay] Added program for Ability to create Address book table in database using SQL

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,1 +1,14 @@
 create database AddressBook
+use AddressBook
+create table Addressbook
+(
+	FirstName	varchar(50),
+	LastName	varchar (50),
+	Address		varchar(150),
+	City		varchar(25),
+	State		varchar(25),
+	zip			BigInt,
+	PhoneNumber BigInt,
+	Email		varchar(50)
+	)
+	SELECT * FROM Addressbook;
